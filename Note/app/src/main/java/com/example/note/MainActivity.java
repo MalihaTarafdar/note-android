0f9fb2719dc2eee5725947b77aa9031f7da07f43
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-05-16 04:56:36 -0400

    Add initial navigation drawer

--- Note/app/src/main/java/com/example/note/MainActivity.java
+++ Note/app/src/main/java/com/example/note/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.note;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -13,24 +16,41 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-	Toolbar toolbar;
+	private DrawerLayout drawer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		drawer = findViewById(R.id.drawer_layout);
+
 		//set toolbar
-		toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		//set toolbar background
 		getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.appbar_bg));
 		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
+				Toast.makeText(MainActivity.this, "Navigation drawer opened", Toast.LENGTH_SHORT).show();
 			}
 		});
+
+		//action bar navigation drawer
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+				R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
 	}
 
 	@Override
