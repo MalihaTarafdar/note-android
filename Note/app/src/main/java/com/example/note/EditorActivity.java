cbf76d3edc6d9cef070f8e353fb96d4a5613c961
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-05-21 05:51:50 -0400

    Add toolbar option selection, style EditText in editor, and refactor colors

--- Note/app/src/main/java/com/example/note/EditorActivity.java
+++ Note/app/src/main/java/com/example/note/EditorActivity.java
@@ -1,10 +1,14 @@
 package com.example.note;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -20,5 +24,27 @@ public class EditorActivity extends AppCompatActivity {
 		//set toolbar up action
 		ActionBar appBar = getSupportActionBar();
 		appBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_editor_find:
+				Toast.makeText(this, "Find item selected", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_editor_export:
+				Toast.makeText(this, "Export item selected", Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menu_editor_info:
+				Toast.makeText(this, "Info item selected", Toast.LENGTH_SHORT).show();
+				return true;
+			default: return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_editor, menu);
+		return true;
 	}
 }
