7c4116024768a5b6b224dea04185258c7aa39f64
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-05-30 04:07:15 -0400

    Remove unused imports

--- Note/app/src/main/java/com/example/note/MainActivity.java
+++ Note/app/src/main/java/com/example/note/MainActivity.java
@@ -1,13 +1,5 @@
 package com.example.note;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,7 +8,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -117,7 +116,6 @@ public class MainActivity extends AppCompatActivity implements SortBottomSheetDi
 				@Override
 				public boolean onQueryTextSubmit(String query) {
 					if (notesFragment != null) notesFragment.filterBySearch(query);
-					Toast.makeText(MainActivity.this, "Query: " + query, Toast.LENGTH_SHORT).show();
 					return false;
 				}
 				@Override
--- Note/app/src/main/java/com/example/note/StorageHelper.java
+++ Note/app/src/main/java/com/example/note/StorageHelper.java
@@ -1,8 +1,6 @@
 package com.example.note;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 
 import java.io.BufferedReader;
